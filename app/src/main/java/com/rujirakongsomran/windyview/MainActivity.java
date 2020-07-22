293de53ebd4183fdd6b35f7fd5a9247158ab95df
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-07-22 16:28:06 +0700

    Write JAVA Code and change color of colorPrimaryDark

--- app/src/main/java/com/rujirakongsomran/windyview/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/windyview/MainActivity.java
@@ -3,6 +3,11 @@ package com.rujirakongsomran.windyview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.ahmadnemati.wind.WindView;
+import com.github.ahmadnemati.wind.enums.TrendType;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnStart = (Button) findViewById(R.id.btnStart);
+        final WindView windView = (WindView) findViewById(R.id.windView);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view == btnStart) {
+                    windView.setPressure(200.0f);
+                    windView.setPressureUnit(" in Hg");
+                    windView.setWindSpeed(100.0f);
+                    windView.setWindSpeedUnit(" kn/h");
+                    windView.setTrendType(TrendType.UP);
+
+                    windView.start();
+
+                }
+            }
+        });
     }
 }
